4beabf3a8496ff3c2bfb34042a3a512a4aab3568
Mateo <mateoarango05@gmail.com>
2024-08-14 17:28:24 -0500

    IMPROVEMENT: events query - from 400ms-500ms to 100-200ms

--- src/modules/events/application/dal/events.dal.ts
+++ src/modules/events/application/dal/events.dal.ts
@@ -8,7 +8,30 @@ export interface EventDAL {
 export const createEventDAL = (knex: Knex): EventDAL => {
   return {
     async getEvents(limit): Promise<Event[]> {
-      return await knex<Event>('events').select('*').limit(limit);
+      /* 
+        Original
+      */
+      // return await knex<Event>('events').select('*').limit(limit);
+
+      /* 
+        From the concept I found, n+1 problem refers to a situation where multiple 
+        database queries are performed instead of a single, more efficient query.
+
+        So, the solution I can think of is to avoid the for loop since it is the one 
+        that causes this problem by calling the getTicketsByEvent function, and optimizing 
+        the sqlquery so that it allows obtaining the data without resorting to n number of extra queries.
+
+        This improvement made the endpoint respond from 400ms-500ms(Soon) to 100ms-150ms(approximately)
+      */
+      const eventsWithTickets = await knex<Event>('events as e')
+        .select('e.*', knex.raw('json_agg(t.*) as availableTickets'))
+        .leftJoin('tickets as t', 'e.id', 't.event_id')
+        .where('t.status', 'available')
+        .groupBy('e.id')
+        .orderBy('e.id')
+        .limit(limit);
+
+      return eventsWithTickets;
     },
   };
 };
--- src/modules/events/infra/controller/get-events.ts
+++ src/modules/events/infra/controller/get-events.ts
@@ -6,11 +6,16 @@ export const createGetEventsController =
   ({ eventsDAL, ticketsDAL }: { eventsDAL: EventDAL; ticketsDAL: TicketsDAL }) =>
   async (_req: Request, res: Response) => {
     const events = await eventsDAL.getEvents(50);
+
+    /* 
+      original
+    */
     //add the available tickets to the response for each event
-    for (let i = 0; i < events.length; i++) {
+    /* for (let i = 0; i < events.length; i++) {
       const event = events[i];
       const tickets = await ticketsDAL.getTicketsByEvent(event.id);
-      events[i].availableTickets = tickets.filter((ticket) => ticket.status === 'available');
-    }
+      events[i].availableTickets = tickets;
+    } */
+
     res.json(events);
   };
